2ce72412695efb38db58a0d3fb76a41dd5bc38ed
Htoo Pyae <htoopyae.dev@gmail.com>
2021-08-15 19:32:09 +0630

    feat: Added desmos calculator

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
-import "./style.css";
+const elt = document.getElementById("calculator");
+const calculator = Desmos.GraphingCalculator(elt);
 
-document.querySelector("#app").innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`;
+calculator.setExpression({ id: "graph1", latex: "y=x^2" });
